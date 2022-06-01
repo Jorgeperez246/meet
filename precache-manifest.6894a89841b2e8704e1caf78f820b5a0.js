19eb1fcd31a65cfc35544764a34e1d2a1ae2a600
Jorgeperez246 <jorgeperez246.jp@gmail.com>
2022-05-31 23:33:43 -0400

    Updates

---
+++ precache-manifest.6894a89841b2e8704e1caf78f820b5a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "20fb2efea020337d344c",
+    "url": "/meet/static/css/main.5e5660e3.chunk.css"
+  },
+  {
+    "revision": "20fb2efea020337d344c",
+    "url": "/meet/static/js/main.21baa2cb.chunk.js"
+  },
+  {
+    "revision": "fdfb3d9364f88da2786b",
+    "url": "/meet/static/js/runtime~main.da053414.js"
+  },
+  {
+    "revision": "ef72a72af5b5b1ac0e44",
+    "url": "/meet/static/js/2.b6a0e90a.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/meet/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e32068e729dd760f48863d8c23f6090d",
+    "url": "/meet/index.html"
+  }
+];
